0998bbb6d12648905cf444cd1bdbecbfe364e14e
wangml <wangmaolin1126@163.com>
2020-02-28 20:37:54 +0800

    Add files via upload

---
+++ 数据结构部分/1-100/_43.cpp
@@ -0,0 +1,179 @@
+﻿#include "normal.h"
+
+/*
+给定两个以字符串形式表示的非负整数 num1 和 num2，返回 num1 和 num2 的乘积，它们的乘积也表示为字符串形式。
+
+num1 和 num2 的长度小于110。
+num1 和 num2 只包含数字 0-9。
+num1 和 num2 均不以零开头，除非是数字 0 本身。
+不能使用任何标准库的大数类型（比如 BigInteger）或直接将输入转换为整数来处理。
+
+来源：力扣（LeetCode）
+链接：https://leetcode-cn.com/problems/multiply-strings
+著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+*/
+
+class Solution {     // 有点慢
+public:
+    string multiply(string num1, string num2) {
+        if (num1[0] == '0' || num2[0] == '0')
+            return "0";
+
+        string result;
+        int len_1 = num1.length();
+        //int len_2 = num2.length();
+        string str_2 = num2;
+
+        for (int i = 0; i < len_1; i++) {
+            int temp = num1[len_1 - 1 - i] - '0';           
+
+            for (int j = 0; j < temp; j++) {
+                result = plus(result, str_2);
+            }
+            str_2.push_back('0');
+        }
+
+        return result;
+    }
+
+    string plus(string num1, string num2) {
+        int len_1 = num1.length();
+        int len_2 = num2.length();
+        int ex = 0;
+        int temp_1 = 0;
+        int i;
+        string result;
+
+        for (i = 0; i < len_1 && i < len_2; i++) {
+            temp_1 = ex + (int)(num1[len_1 - 1 - i] - '0') + (int)(num2[len_2 - 1 -i] - '0');
+            ex = temp_1 / 10;
+            temp_1 = temp_1 - 10 * ex;
+            result.push_back((char)('0' + temp_1));
+        }
+
+        while (i < len_1) {
+            temp_1 = ex + (int)(num1[len_1 - 1 - i] - '0');
+            ex = temp_1 / 10;
+            temp_1 = temp_1 - 10 * ex;
+            result.push_back((char)('0' + temp_1));
+            i++;
+        }
+
+        while (i < len_2) {
+            temp_1 = ex + (int)(num2[len_2 - 1 - i] - '0');
+            ex = temp_1 / 10;
+            temp_1 = temp_1 - 10 * ex;
+            result.push_back((char)('0' + temp_1));
+            i++;
+        }
+
+        if (ex != 0)
+            result.push_back((char)('0' + ex));
+
+        int len_3 = result.length();
+        for (int j = 0; j < len_3 / 2; j++) {
+            char ch = result[j];
+            result[j] = result[len_3 - 1 - j];
+            result[len_3 - 1 - j] = ch;
+        }
+
+        return result;
+    }
+};
+
+
+class Solution {     // 优化上面的解决方案，主要优化第二个for语句
+public:              // 运行结果显示运行时间是上述方案的四分之一，运行内存是上述方案的一半，但还是比较慢啊
+    string multiply(string num1, string num2) {
+        if (num1[0] == '0' || num2[0] == '0')
+            return "0";
+
+        string result;
+        int len_1 = num1.length();
+        //int len_2 = num2.length();
+        string str_2 = num2;
+
+        for (int i = 0; i < len_1; i++) {
+            int temp = num1[len_1 - 1 - i] - '0';
+
+            result = plus(result, mlt(str_2, temp));
+            
+            str_2.push_back('0');
+        }
+
+        return result;
+    }
+
+    string plus(string num1, string num2) {
+        int len_1 = num1.length();
+        int len_2 = num2.length();
+        int ex = 0;
+        int temp_1 = 0;
+        int i;
+        string result;
+
+        for (i = 0; i < len_1 && i < len_2; i++) {
+            temp_1 = ex + (int)(num1[len_1 - 1 - i] - '0') + (int)(num2[len_2 - 1 - i] - '0');
+            ex = temp_1 / 10;
+            temp_1 = temp_1 - 10 * ex;
+            result.push_back((char)('0' + temp_1));
+        }
+
+        while (i < len_1) {
+            temp_1 = ex + (int)(num1[len_1 - 1 - i] - '0');
+            ex = temp_1 / 10;
+            temp_1 = temp_1 - 10 * ex;
+            result.push_back((char)('0' + temp_1));
+            i++;
+        }
+
+        while (i < len_2) {
+            temp_1 = ex + (int)(num2[len_2 - 1 - i] - '0');
+            ex = temp_1 / 10;
+            temp_1 = temp_1 - 10 * ex;
+            result.push_back((char)('0' + temp_1));
+            i++;
+        }
+
+        if (ex != 0)
+            result.push_back((char)('0' + ex));
+
+        int len_3 = result.length();
+        for (int j = 0; j < len_3 / 2; j++) {
+            char ch = result[j];
+            result[j] = result[len_3 - 1 - j];
+            result[len_3 - 1 - j] = ch;
+        }
+
+        return result;
+    }
+
+    string mlt(string num1, int cnt) {   // 字符串表示的数字num1与仅有一位的数cnt相乘
+        if (cnt == 0)
+            return "0";
+
+        int len = num1.length();
+        int ex = 0;
+        string result;
+
+        for (int i = 0; i < len; i++) {
+            int temp_1 = num1[len - 1 - i] - '0';
+            int temp_2 = ex + cnt * temp_1;
+            ex = temp_2 / 10;
+            temp_2 = temp_2 - ex * 10;
+            result.push_back((char)('0' + temp_2));
+        }
+
+        if (ex != 0)
+            result.push_back((char)('0' + ex));
+
+        int len_3 = result.length();
+        for (int j = 0; j < len_3 / 2; j++) {
+            char ch = result[j];
+            result[j] = result[len_3 - 1 - j];
+            result[len_3 - 1 - j] = ch;
+        }
+
+        return result;
+    }
+};
---
+++ 数据结构部分/1-100/_44.cpp
@@ -0,0 +1,141 @@
+﻿#include "normal.h"
+
+/*
+给定一个字符串 (s) 和一个字符模式 (p) ，实现一个支持 '?' 和 '*' 的通配符匹配。
+
+'?' 可以匹配任何单个字符。
+'*' 可以匹配任意字符串（包括空字符串）。
+两个字符串完全匹配才算匹配成功。
+
+说明:
+
+s 可能为空，且只包含从 a-z 的小写字母。
+p 可能为空，且只包含从 a-z 的小写字母，以及字符 ? 和 *。
+
+来源：力扣（LeetCode）
+链接：https://leetcode-cn.com/problems/wildcard-matching
+著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+*/
+
+class Solution {    // 超时
+public:
+    bool isMatch(string s, string p) {
+        bool one = false;
+
+        isMatch_x(s, p, one);
+
+        return one;
+    }
+
+    bool isMatch_x(string s, string p, bool& one) {
+        if (one)
+            return true;
+
+        if (s.empty()) {
+            if (p.empty()) {
+                one = true;
+                return true;
+            }
+            else {
+                int i = 0;
+                while (p[i] == '*' && i < p.length())
+                    i++;
+
+                if (i == p.length()) {
+                    one = true;
+                    return true;
+                }
+                else
+                    return false;
+            }
+        }
+
+        if (!s.empty() && p.empty())
+            return false;
+        
+        if (p[0] == '?') {
+            isMatch_x(s.substr(1), p.substr(1), one);
+        }
+        else if (p[0] == '*') {   // 一个星号和n个星号表示同样的意思
+            int cnt = 0;
+            while (p[cnt] == '*' && cnt < p.length())
+                cnt++;
+
+            for (int i = 0; i <= s.length(); i++) {
+                isMatch_x(s.substr(i), p.substr(cnt), one);
+            }               
+        }
+        else {
+            if (s[0] == p[0])
+                isMatch_x(s.substr(1), p.substr(1), one);
+        }
+
+        return false;
+    }
+};
+
+
+
+class Solution {     // 还是超时
+public:
+    bool isMatch(string s, string p) {
+        if (p.empty())
+            return s.empty();
+
+        if (s.empty()) {
+            int i = 0;
+            while (p[i] == '*' && i < p.length())
+                i++;
+
+            if (i == p.length()) {
+                return true;
+            }
+            else
+                return false;
+        }
+        else {
+            if (s[0] == p[0] || p[0] == '?')
+                return isMatch(s.substr(1), p.substr(1));
+            else if (p[0] == '*') {
+                int cnt = 0;
+                while (p[cnt] == '*' && cnt < p.length())
+                    cnt++;
+
+                return isMatch(s, p.substr(cnt)) || isMatch(s.substr(1), p.substr(cnt - 1));
+            }
+            else
+                return false;
+        }
+    }
+};     // 除了清理星号，还要记录下已测试过的步骤，防止重复
+
+
+// 动态规划
+class Solution {
+public:
+    bool isMatch(string s, string p) {
+        int len_s = s.length();
+        int len_p = p.length();
+
+        vector<vector<bool>> dp(len_s + 1, vector<bool>(len_p + 1, false));
+        // 初始化
+        dp[0][0] = true;
+        for (int i = 1; i <= len_p; i++) {    // 用s的空匹配p，填dp第0行
+            dp[0][i] = dp[0][i - 1] && p[i - 1] == '*';
+        }
+
+        for (int i = 1; i <= len_s; i++) {    // 依次填dp，从第一行往后
+            for (int j = 1; j <= len_p; j++) {
+                if (s[i - 1] == p[j - 1] || p[j - 1] == '?')
+                    dp[i][j] = dp[i - 1][j - 1];
+                else if (p[j - 1] == '*')
+                    dp[i][j] = dp[i][j - 1] || dp[i - 1][j];    // 星号做空字符或代替掉s的n个字符
+                else
+                    ;
+            }
+        }
+        
+        return dp[len_s][len_p];   // 最后结果
+    }
+};
+

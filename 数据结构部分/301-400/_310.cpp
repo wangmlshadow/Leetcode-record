e4fd07fdd01469132298f98e128e381fc18acdca
wangml <wangmaolin1126@163.com>
2020-05-22 04:55:11 -0500

    Add files via upload

---
+++ 数据结构部分/301-400/_310.cpp
@@ -0,0 +1,75 @@
+﻿#include "normal.h"
+
+/*
+对于一个具有树特征的无向图，我们可选择任何一个节点作为根。图因此可以成为树，在所有可能的树中，具有最小高度的树被称为最
+小高度树。给出这样的一个图，写出一个函数找到所有的最小高度树并返回他们的根节点。
+
+格式
+
+该图包含 n 个节点，标记为 0 到 n - 1。给定数字 n 和一个无向边 edges 列表（每一个边都是一对标签）。
+
+你可以假设没有重复的边会出现在 edges 中。由于所有的边都是无向边， [0, 1]和 [1, 0] 是相同的，因此不会同时出现在 edges 里。
+
+来源：力扣（LeetCode）
+链接：https://leetcode-cn.com/problems/minimum-height-trees
+著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+*/
+
+class Solution {   // 拓扑排序
+public:
+    vector<int> findMinHeightTrees(int n, vector<vector<int>>& edges) {
+        if (n == 1)
+            return { 0 };
+
+        if (n == 2)
+            return { 0, 1 };
+
+        vector<int> indegree(n, 0);
+        vector<vector<int>> graph(n, vector<int>(0));
+
+        for (int i = 0; i < edges.size(); i++) {
+            graph[edges[i][0]].push_back(edges[i][1]);
+            graph[edges[i][1]].push_back(edges[i][0]);
+            indegree[edges[i][0]]++;
+            indegree[edges[i][1]]++;
+        }
+
+        queue<int> q;
+
+        for (int i = 0; i < n; i++)
+            if (indegree[i] == 1)
+                q.push(i);
+
+        int cntOne = q.size();
+
+        while (n > 2) {
+            n -= cntOne;
+
+            while (cntOne-- != 0) {   // 一次性删除所有入度为1的节点
+                int temp = q.front();
+                q.pop();
+                indegree[temp] = 0;
+
+                for (int i = 0; i < graph[temp].size(); i++) {
+                    if (indegree[graph[temp][i]] != 0) {
+                        indegree[graph[temp][i]]--;
+
+                        if (indegree[graph[temp][i]] == 1)
+                            q.push(graph[temp][i]);
+                    }
+                }
+            }
+
+            cntOne = q.size();
+        }
+
+        vector<int> res;
+
+        while (!q.empty()) {
+            res.push_back(q.front());
+            q.pop();
+        }
+
+        return res;
+    }
+};
---
+++ 数据结构部分/301-400/_312.cpp
@@ -0,0 +1,73 @@
+﻿#include "normal.h"
+
+/*
+有 n 个气球，编号为0 到 n-1，每个气球上都标有一个数字，这些数字存在数组 nums 中。
+
+现在要求你戳破所有的气球。每当你戳破一个气球 i 时，你可以获得 nums[left] * nums[i] * nums[right] 个硬
+币。 这里的 left 和 right 代表和 i 相邻的两个气球的序号。注意当你戳破了气球 i 后，气球 left 和气
+球 right 就变成了相邻的气球。
+
+求所能获得硬币的最大数量。
+
+说明:
+
+你可以假设 nums[-1] = nums[n] = 1，但注意它们不是真实存在的所以并不能被戳破。
+0 ≤ n ≤ 500, 0 ≤ nums[i] ≤ 100
+
+来源：力扣（LeetCode）
+链接：https://leetcode-cn.com/problems/burst-balloons
+著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+*/
+
+class Solution {   // 递归在第30个测试案例超时
+public:
+    int maxCoins(vector<int>& nums) {
+        int n = nums.size() + 2;
+        vector<int> newNums(n, 1);
+
+        for (int i = 0; i < nums.size(); i++)
+            newNums[i + 1] = nums[i];
+
+        vector<vector<int>> res(n, vector<int>(n, 0));
+        return dp(res, newNums, 0, n - 1);
+    }
+
+    int dp(vector<vector<int>>& res, vector<int>& newNums, int left, int right) {
+        if (left + 1 == right)
+            return 0;
+
+        if (res[left][right] > 0)
+            return res[left][right];
+
+        int ans = 0;
+
+        for (int i = left + 1; i < right; i++) {
+            ans = max(ans, newNums[left] * newNums[i] * newNums[right] + dp(res, newNums, left, i) + dp(res, newNums, i, right));
+        }
+
+        return ans;
+    }
+};
+
+class Solution {  // 这个过了 但是也很慢啊
+public:
+    int maxCoins(vector<int>& nums) {
+        int n = nums.size() + 2;
+        vector<int> newNums(n, 1);
+
+        for (int i = 0; i < nums.size(); i++)
+            newNums[i + 1] = nums[i];
+
+        vector<vector<int>> dp(n, vector<int>(n, 0));
+        
+        for (int left = n - 2; left > -1; left--) {
+            for (int right = left + 2; right < n; right++) {
+                for (int i = left + 1; i < right; i++) {
+                    dp[left][right] = max(dp[left][right], newNums[left] * newNums[i] * newNums[right] + dp[left][i] + dp[i][right]);
+                }
+            }
+        }
+
+        return dp[0][n - 1];
+    }
+};

ca01cf3075f21e288e877ea46b767f0eefc3d29d
wangml <wangmaolin1126@163.com>
2020-08-10 16:26:33 +0800

    Add files via upload

---
+++ 按分类/动态规划/_1140-.cpp
@@ -0,0 +1,45 @@
+﻿#include "normal.h"
+
+/*
+动态规划
+
+亚历克斯和李继续他们的石子游戏。许多堆石子 排成一行，每堆都有正整数颗石子 piles[i]。游戏以谁手中的石子最多来决出胜负。
+
+亚历克斯和李轮流进行，亚历克斯先开始。最初，M = 1。
+
+在每个玩家的回合中，该玩家可以拿走剩下的 前 X 堆的所有石子，其中 1 <= X <= 2M。然后，令 M = max(M, X)。
+
+游戏一直持续到所有石子都被拿走。
+
+假设亚历克斯和李都发挥出最佳水平，返回亚历克斯可以得到的最大数量的石头。
+
+*/
+
+class Solution {
+public:
+    int stoneGameII(vector<int>& piles) {
+        vector<vector<int>> dp(piles.size(), vector<int>(piles.size() + 1, 0));
+        int sum = 0;
+
+        for (int i = piles.size() - 1; i >= 0; i--) {
+            sum += piles[i];
+
+            for (int M = 1; M <= piles.size(); M++) {
+                if (i + 2 * M >= piles.size())
+                    dp[i][M] = sum;
+                else {
+                    for (int x = 1; x <= 2 * M; x++)
+                        dp[i][M] = max(dp[i][M], sum - dp[i + x][max(M, x)]);
+                }
+            }
+        }
+
+        return dp[0][1];
+    }
+};
+
+/*
+执行结果：通过 显示详情
+执行用时：12 ms, 在所有 C++ 提交中击败了58.71%的用户
+内存消耗：7.3 MB, 在所有 C++ 提交中击败了85.29%的用户
+*/
---
+++ 按分类/动态规划/_964.cpp
@@ -0,0 +1,70 @@
+﻿#include "normal.h"
+
+/*
+动态规划
+
+给定一个正整数 x，我们将会写出一个形如 x (op1) x (op2) x (op3) x ... 的表达式，其中每个运
+算符 op1，op2，… 可以是加、减、乘、除（+，-，*，或是 /）之一。例如，对于 x = 3，我们可以
+写出表达式 3 * 3 / 3 + 3 - 3，该式的值为 3 。
+
+在写这样的表达式时，我们需要遵守下面的惯例：
+
+除运算符（/）返回有理数。
+任何地方都没有括号。
+我们使用通常的操作顺序：乘法和除法发生在加法和减法之前。
+不允许使用一元否定运算符（-）。例如，“x - x” 是一个有效的表达式，因为它只使用减法，但
+是 “-x + x” 不是，因为它使用了否定运算符。 
+我们希望编写一个能使表达式等于给定的目标值 target 且运算符最少的表达式。返回所用运算符
+的最少数量。
+
+思路：要找到最少数量，那么显示x不断相乘接近target，再求剩下部分数字如何表示
+
+已知：
+x^n < target <x^(n+1)
+
+令x^n+a=target,x^(n+1)-b=target
+
+令left = f(target),right = f(b)
+
+则可知 target + b = x^(n+1)
+
+当 target<b的情况下，left<=right.
+*/
+
+class Solution {
+public:
+    int leastOpsExpressTarget(int x, int target) {
+        return dp(x, target);
+    }
+
+private:
+    map<int, int> memo;
+
+    int dp(int x, int target) {
+        if (memo.find(target) != memo.end())
+            return memo[target];
+
+        if (target < x)
+           return memo[target] = min(2 * target - 1, (x - target) * 2);   // 最小单元
+
+        if (target == 0)
+            return memo[0] = 0;
+
+        double tmp = log((double)target) / log((double)x);
+        long long p = (long long)tmp;
+        long long sum = pow(x, p);
+        long long ans = dp(x, target - sum) + p;
+
+        // target > b 时 可能left > right
+        if (sum * x - target < target)
+            ans = min(ans, p + 1 + dp(x, sum * x - target));
+
+        return memo[target] = ans;
+    }
+};
+
+/*
+执行结果：通过 显示详情
+执行用时：4 ms, 在所有 C++ 提交中击败了94.23%的用户
+内存消耗：9.3 MB, 在所有 C++ 提交中击败了20.00%的用户
+*/

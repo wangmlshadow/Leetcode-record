f54b228a5c8e9aa2728532541d7ca4c6dfed3d66
wangml <wangmaolin1126@163.com>
2021-05-19 10:19:02 +0800

    Create 区间交集问题.py

---
+++ labuladong的算法小抄(Python)/区间交集问题.py
@@ -0,0 +1,29 @@
+# 第986题
+# 给定两个由一些 闭区间 组成的列表，firstList 和 secondList ，其中 firstList[i] = [starti, endi] 而 secondList[j] = [startj, endj] 。每个区间列表都是成对 不相交 的，并且 已经排序 。
+# 返回这 两个区间列表的交集 。
+# 形式上，闭区间 [a, b]（其中 a <= b）表示实数 x 的集合，而 a <= x <= b 。
+# 两个闭区间的 交集 是一组实数，要么为空集，要么为闭区间。例如，[1, 3] 和 [2, 4] 的交集为 [2, 3] 。
+
+class Solution:
+    def intervalIntersection(self, firstList: List[List[int]], secondList: List[List[int]]) -> List[List[int]]:
+        i, j = 0, 0# 双指针
+        res = []
+        while i < len(firstList) and j < len(secondList):
+            a1, a2 = firstList[i][0], firstList[i][1]
+            b1, b2 = secondList[j][0], secondList[j][1]
+            # 两个区间存在交集
+            if b2 >= a1 and a2 >= b1:
+                # 计算交集 加入res
+                res.append([max(a1, b1), min(a2, b2)])
+            # 指针前进
+            if b2 < a2:
+                j += 1
+            else:
+                i += 1
+        return res
+"""
+执行结果：通过
+显示详情
+执行用时：48 ms, 在所有 Python3 提交中击败了95.11%的用户
+内存消耗：15.3 MB, 在所有 Python3 提交中击败了65.38%的用户
+"""
